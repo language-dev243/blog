6f91617288d208d7210e3ecdeb59934a5b3bdb94
language-dev243 <language-dev@protonmail.com>
2025-02-19 02:49:56 +0100

    adding tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,13 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap()]
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
